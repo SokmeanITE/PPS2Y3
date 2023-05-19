dc92164aa6a46486021487d1942ad77131ffe433
Sokmean <sokmean@Sokmeans-MacBook-Pro.local>
2023-05-19 12:59:36 +0700

    pps2y3

--- src/Authentication/Signup.jsx
+++ src/Authentication/Signup.jsx
@@ -2,40 +2,28 @@ import React from 'react';
 
 function Signup() {
   return (
-    <form>
-      <h3>Sign Up</h3>
-      <div class="container  mt-5 w-50">
-        <form>
-            <div><h3>Register Form</h3></div>
-            <div class="mb-3">
-              <label  class="form-label">Email *</label>
-              <input type="email" id="email" class="form-control" placeholder="Email email"  aria-describedby="emailHelp"/>
-              <p class="text-danger" id="erroremail"></p>
+    <div class="container">
+    <div class="row">
+        <div class="offset-md-2 col-lg-5 col-md-7 offset-lg-4 offset-md-3">
+            <div class="panel border bg-white">
+                <div class="panel-heading">
+                    <h3 class="pt-3 font-weight-bold">Login</h3>
+                </div>
+                <div class="panel-body p-3">
+                    <form action="login_script.php" method="POST">
+                        <div class="form-group py-2">
+                            <div class="input-field"> <span class="far fa-user p-2"></span> <input type="email" placeholder="Email" required="&nbsp"/> </div>
+                        </div>
+                        <div class="form-group py-1 pb-2">
+                            <div class="input-field"> <span class="fas fa-lock px-2"></span> <input type="password" placeholder="Enter your Password" required/> <button class="btn bg-white text-muted"> <span class="far fa-eye-slash"></span> </button> </div>
+                        </div>
+                        <div class="btn btn-primary btn-block mt-3">Signup</div>
+                    </form>
+                </div>
             </div>
-
-            <div class="mb-3">
-              <label  class="form-label">Password</label>
-              <input type="password" id="pass" class="form-control" placeholder="Enter password"/>
-            </div>
-            <p class="text-danger" id="errorpassword"></p>
-            
-            <div class="mb-3">
-                <label  class="form-label">Fullname *</label>
-                <input type="email" id="name" class="form-control" placeholder="Enter lastname firstname"  aria-describedby="emailHelp"/>
-              </div>
-              <p class="text-danger" id="errorname"></p>
-
-            <div class="mb-3">
-                <label  class="form-label">Phone *</label>
-                <input type="email" id="phone" class="form-control" placeholder="Enter phone"  aria-describedby="emailHelp"/>
-            </div>
-            <p class="text-danger" id="errorphone"></p>
-
-            <button type="submit" id="btn-submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
     </div>
-
-    </form>
+</div>
   );
 }
 
